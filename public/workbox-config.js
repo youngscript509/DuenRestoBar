b8465c47d2559cf3e995136a26419ce7995cdf0c
wedscriptninja <84065772+wedscriptninja@users.noreply.github.com>
2024-04-02 10:41:54 -0400

    2

---
+++ public/workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    globDirectory: 'build/',
+    globPatterns: ['**/*.{html,js,css,png,jpg,json}'],
+    swDest: 'build/sw.js',
+    clientsClaim: true,
+    skipWaiting: true,
+  };
+  
