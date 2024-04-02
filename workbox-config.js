e3bf2d323667330e22172bd2a1ac2e2f631f3e70
wedscriptninja <84065772+wedscriptninja@users.noreply.github.com>
2024-04-02 12:04:47 -0400

    last1

--- workbox-config.js
+++ workbox-config.js
@@ -2,6 +2,24 @@ module.exports = {
   globDirectory: 'build/',
   globPatterns: ['**/*.{html,js,css,png,jpg,json}'],
   swDest: 'build/sw.js',
-  clientsClaim: true,
-  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/api.example.com\/data/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'api-data',
+      },
+    },
+  ],
 };
